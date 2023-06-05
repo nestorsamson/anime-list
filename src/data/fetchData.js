33bbb01a8c565697b8516cbf8f46b196625b5f4c
nestorsamson <nestorsamson99@gmail.com>
2023-06-05 10:10:28 +0000

    initial commit

---
+++ src/data/fetchData.js
@@ -0,0 +1,20 @@
+const fetchData = async (path)=>{
+    const source = 'https://api.jikan.moe/v4/'
+
+    try{
+        let data = await fetch(source+path,{
+            
+        })
+        if(!data){
+            throw Error('No data')
+
+        }
+        return data.json()
+
+    }catch(e){
+        error.value = e.message
+    }
+
+
+}
+export default fetchData
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+   <div v-for="anime in data" :key="anime.mal_id">
+    {{ anime.entry[0].title}}
+    <img :src="anime.entry[0].images.jpg.image_url">
+   </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import fetchData from '@/data/fetchData'
+import { ref } from 'vue'
+
+
+export default ({
+  name: 'HomeView',
+  components: {
+  },
+  setup(){
+    const data = ref()
+    fetchData('recommendations/anime')
+    .then(response =>{
+      data.value = response.data
+      console.log(data.value)
+    } )
+    
+    return {data}
+  }
+})
+</script>
